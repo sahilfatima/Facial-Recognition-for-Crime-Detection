7285e917ee1b33620eaa47a27186fc07511e1810
Sahil Fatima <141179460+sahilfatima@users.noreply.github.com>
2024-05-26 03:29:23 -0700

    Update face_detection.py
    Consise and readable

--- face_detection.py
+++ face_detection.py
@@ -1,4 +1,3 @@
-import numpy as np 
 import cv2
 # from final_encoding import *
 
@@ -7,22 +6,24 @@ def detect(path):
     #cam = cv2.VideoCapture(0)
     cam = cv2.VideoCapture(path)
     sampleNum = 0
-
-
-    while(True):
-        ret,img = cam.read()
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        faces = facedetect.detectMultiScale(gray,1.3,5)
-
-        for(x,y,w,h) in faces:
-            sampleNum+=1
-            cv2.imwrite('dataset/'+str(sampleNum)+'.jpg',gray[y:y+h,x:x+w])
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.waitKey(1)
-        cv2.imshow('face',img)
-        cv2.waitKey(1)
-        if(sampleNum>100):
+   
+    while sampleNum <= 100:
+        ret, img = cam.read()
+        if not ret:
             break
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = facedetect.detectMultiScale(gray, 1.3, 5)
+
+     for (x, y, w, h) in faces:
+            sampleNum += 1
+            cv2.imwrite(f'dataset/{sampleNum}.jpg', gray[y:y+h, x:x+w])
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            if sampleNum > 100:
+                break
+
+    cv2.imshow('face', img)
+        if cv2.waitKey(1) & 0xFF == ord('q') or sampleNum > 100:
+            break
     cam.release()
     cv2.destroyAllWindows()
